1a4935bb588acb2553d1c291d6040eebaff00c6f
Jesse Essel <esseljesse@outlook.com>
2025-07-09 15:48:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Defining success
+def success(consistency, dedication, passion):
+    # Conditional Statement
+    if consistency and dedication and passion:
+        print("\nResult:\nConsistency, dedication, and passion are the gateway to success as a programmer.")
+    else:
+        print("\nCaution:\nStay committed and passionate to achieve success.")
+
+# Display the output
+success(True, True,True)
